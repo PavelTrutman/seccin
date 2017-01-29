0965d2abea2d369999ef8accdb571a0c9c08656a
Pavel Trutman <pavel.trutman@fel.cvut.cz>
2017-01-29 21:04:10 +0100

    Resolving overwriting of already created coffin.

--- seccin.py
+++ seccin.py
@@ -1,7 +1,39 @@
-#!/usr/bin/python3
+#!/usr/bin/python3.6
 
+import sys
 import argparse
+from os.path import normpath
 from pathlib import Path
+
+def queryYesNo(question, default=None):
+  """
+  Ask a yes/no question via raw_input() and return their answer.
+
+  Args:
+    question (str): a string that is presented to the user
+    default (str): the presumed answer if the user just hits <Enter>. It must be 'yes', 'no' or None (default) (meaning an answer is required of the user)
+
+  Returns:
+    bool: True for 'yes' or False for 'no'
+  """
+
+  valid = {"yes": True, "y": True, "no": False, "n": False}
+  if default is None:
+    prompt = " [y/n] "
+  elif default == "yes":
+    prompt = " [Y/n] "
+  elif default == "no":
+    prompt = " [y/N] "
+  else:
+    raise ValueError("invalid default answer: '%s'" % default)
+
+  while True:
+    sys.stdout.write(question + prompt)
+    choice = input().lower()
+    if default is not None and choice == '':
+      return valid[default]
+    elif choice in valid:
+      return valid[choice]
 
 # argument parsing
 parser = argparse.ArgumentParser()
@@ -11,4 +43,21 @@ parser.description = 'seccin - Secret in Coffin\n\r  Tool to encrypt passwords a
 parser.add_argument('path', nargs='?', default=Path.cwd().joinpath('coffin'), type=str, help='path to the crypted coffin')
 parser.add_argument('--init', '-i', action='store_true', help='create new crypted coffin')
 args = parser.parse_args()
-print(args)
+
+coffin = Path(normpath(str(args.path)))
+init = args.init
+
+print(coffin)
+print(type(coffin))
+print(init)
+
+if init:
+  
+  # check that coffin exists
+  if coffin.exists():
+    if not queryYesNo('The coffin at ' + str(coffin) + ' already exists. Do you want to overwrite it?', 'no'):
+      # not overwriting, exiting
+      print('exit')
+  
+  
+
